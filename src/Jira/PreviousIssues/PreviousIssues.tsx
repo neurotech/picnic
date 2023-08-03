a5a9c9c037d24b1fce801407d3729c3849da6076
Tim Douglas <neurotech@gmail.com>
2023-08-03 20:54:10 +1000

    Removed unused code

--- src/Jira/PreviousIssues/PreviousIssues.tsx
+++ src/Jira/PreviousIssues/PreviousIssues.tsx
@@ -1,8 +1,5 @@
 import styled from "@emotion/styled";
-import { palette } from "../../theme/palette";
 import { Issue } from "./Issue";
-import { useState } from "react";
-import { keyframes } from "@emotion/react";
 import { Stack } from "../../layout/Stack";
 import { Button } from "../../Button";
 import { Dossier } from "../../Dossier";
@@ -18,68 +15,6 @@ export interface Issue {
   text: string;
   timestamp: Date;
 }
-
-interface CopiedProps {
-  copied: boolean;
-}
-
-const IssuesContainer = styled.div``;
-
-const IssuesHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const IssuesHeaderText = styled.div`
-  color: ${palette.white.main};
-  background-color: ${palette.blue.main};
-  width: fit-content;
-  padding: 0.25rem 0.5rem;
-  border-radius: 3px 3px 0 0;
-  user-select: none;
-`;
-
-const fade = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
-const Copied = styled.div<CopiedProps>`
-  user-select: none;
-  color: ${palette.yellow.dark};
-  background-color: ${palette.yellow.main};
-  font-weight: 500;
-  border-radius: 3px;
-  font-size: 11px;
-  line-height: 11px;
-  padding: 0.25rem 0.5rem;
-
-  animation-direction: ${(props) => (props.copied ? "backwards" : "normal")};
-  animation-name: ${fade};
-  animation-duration: 1s;
-  animation-timing-function: ease;
-`;
-
-const IssuesBody = styled.div`
-  border: 2px solid ${palette.blue.main};
-  border-radius: 0 3px 3px 3px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const IssuesFooter = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-`;
-const IssuesFooterText = styled.div`
-  width: fit-content;
-  padding: 0.15rem 0 0 0;
-`;
 
 const NoIssuesFound = styled.div`
   background-color: ${(props) => props.theme.issues.base.background};
